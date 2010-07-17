6bc8eb1bd353099270e25a1245bb8e816400f3b1
Phil McClure <phil.mcclure@gmail.com>
2010-07-17 16:55:54 +0100

    Added Gemfile and some basic tests

--- lib/beer_mapping/rating.rb
+++ lib/beer_mapping/rating.rb
@@ -1,6 +1,6 @@
 module BeerMapping
   class Rating
-    attr_reader :overall, :selection, :service, :food, :reviewcount, :fbscore, :fbcount
+    attr_reader :overall, :selection, :atmosphere, :service, :food, :reviewcount, :fbscore, :fbcount
 
     def initialize(overall, selection, service, atmosphere, food, reviewcount, fbscore, fbcount)
       @overall, @selection, @service, @atmosphere, @food, @reviewcount, @fbscore, @fbcount = overall, selection, service, atmosphere, food, reviewcount, fbscore, fbcount
--- test/test_beer_mapping.rb
+++ test/test_beer_mapping.rb
@@ -1,7 +1,38 @@
 require 'helper'
+TEST_KEY = 'bdf79cbc8aa8d866b77dccfe9349815e'
 
 class TestBeerMapping < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  context "A new beer search" do
+    setup do
+      @beer = BeerMapping::API.new(TEST_KEY)
+      @beer.find_by_location("chicago")
+    end
+
+    should "return a pub from chicago when searching by location, chicago" do
+      assert_equal @beer.locations[0].city, 'Chicago'
+    end
+
+    should "have a geo location" do
+      assert !@beer.locations[0].geo.nil?
+    end
+
+    should "have image attributes if an image is present" do
+      if !@beer.locations[0].images.nil?
+        assert !@beer.locations[0].images[0].url.nil?
+        assert !@beer.locations[0].images[0].thumburl.nil?
+        assert !@beer.locations[0].images[0].caption.nil?
+        assert !@beer.locations[0].images[0].height.nil?
+        assert !@beer.locations[0].images[0].width.nil?
+      end
+    end
+
+    should "have rating attributes if a rating is present" do
+      if !@beer.locations[0].ratings.nil?
+        assert !@beer.locations[0].ratings.service.nil?
+        assert !@beer.locations[0].ratings.atmosphere.nil?
+        assert !@beer.locations[0].ratings.selection.nil?
+        assert !@beer.locations[0].ratings.overall.nil?
+      end
+    end
   end
 end
